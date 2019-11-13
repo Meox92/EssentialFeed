9f1b6892d45070779533674ff8272df46e4b9690
meox92@gmail.com <meox92@gmail.com>
2019-11-13 22:10:59 +0100

    Initial Commit

---
+++ EssentialFeedMac/EssentialFeedMac.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeedMac.h
+//  EssentialFeedMac
+//
+//  Created by Maola Ma on 13/11/2019.
+//  Copyright © 2019 Maola. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedMac.
+FOUNDATION_EXPORT double EssentialFeedMacVersionNumber;
+
+//! Project version string for EssentialFeedMac.
+FOUNDATION_EXPORT const unsigned char EssentialFeedMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedMac/PublicHeader.h>
+
+
